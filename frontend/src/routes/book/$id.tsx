e685f6c1a0ed17813ecba4728e4969b8e21a9e82
Elias.Egel <elias.egelrid@gmail.com>
2025-06-05 09:41:40 +0200

    Made user unable to see bookpage unless user is logged in

--- frontend/src/routes/book/$id.tsx
+++ frontend/src/routes/book/$id.tsx
@@ -1,12 +1,25 @@
-import { createFileRoute } from '@tanstack/react-router'
-import BookPage from '../../components/book/BookPage'
+import { createFileRoute } from "@tanstack/react-router";
+import BookPage from "../../components/book/BookPage";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
 
-export const Route = createFileRoute('/book/$id')({
+export const Route = createFileRoute("/book/$id")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  const {id}=Route.useParams(); 
+  const { id } = Route.useParams();
 
-  return <div><BookPage id={id}/></div>
+  return (
+    <div>
+      <SignedIn>
+        <BookPage id={id} />
+      </SignedIn>
+      <SignedOut>
+        <p className="text-lg mb-4">You must be signed in to view this page.</p>
+        <SignInButton>
+          <button className="btn btn-primary">Sign In</button>
+        </SignInButton>
+      </SignedOut>
+    </div>
+  );
 }
